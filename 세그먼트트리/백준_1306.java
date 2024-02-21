dce8839b2782a2eade17d02600472f48e41b978a
Seongyu <gcael@naver.com>
2024-02-21 12:32:02 +0900

    경로 이동

--- 세그먼트트리/백준_1306.java
+++ 세그먼트트리/백준_1306.java
@@ -1,4 +1,4 @@
-package 자료구조;
+package 세그먼트트리;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- 세그먼트트리/백준_2042.java
+++ 세그먼트트리/백준_2042.java
@@ -1,16 +1,14 @@
-package 자료구조;
+package 세그먼트트리;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class 백준_11505 {
+public class 백준_2042 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        final int value = 1000000007;
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());
@@ -22,38 +20,41 @@ public class 백준_11505 {
 
         long[] tree = new long[size];
         int startIndex = (int)Math.pow(2, height);
-        Arrays.fill(tree, 1);
 
         for(int i = 0 ; i < N ; i++){
-            tree[startIndex + i] = Integer.parseInt(br.readLine());
+            tree[startIndex + i] = Long.parseLong(br.readLine());
         }
 
         for(int i = startIndex - 1 ; i>= 1 ; i--){
-            tree[i] = (tree[i * 2] % value * tree[i * 2 + 1] % value) % value;
+            tree[i] = tree[i * 2] + tree[i * 2 + 1];
         }
 
         for(int i = 0 ; i < M + K ; i++){
             st = new StringTokenizer(br.readLine());
-            int a= Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            long answer = 1;
+            long c = Long.parseLong(st.nextToken());
+            long answer = 0;
 
-            if(a==1){
+            if(a == 1){
                 b = startIndex + b - 1;
-                tree[b] = c % value;
-                while(b > 1){
-                    b = b/2;
-                    tree[b] = (tree[b * 2] % value * tree[b * 2 + 1] % value) % value;
+
+                long value = c - tree[b];
+                tree[b] = c;
+                while(b > 0){
+                    b = b / 2;
+                    tree[b] += value;
                 }
             }else{
+
                 b = startIndex + b - 1;
                 c = startIndex + c - 1;
-                while(b <= c){
-                    if(b % 2 == 1) answer = answer * tree[b] % value;
-                    if(c % 2== 0) answer = answer * tree[c] % value;
-                    b = (b+1) / 2;
-                    c = (c-1) / 2;
+
+                while(b<=c){
+                    if(b % 2 == 1) answer += tree[b];
+                    if(c % 2 == 0) answer += tree[(int)c];
+                    b = (b + 1) / 2;
+                    c = (c - 1) / 2;
                 }
                 sb.append(answer).append("\n");
             }
