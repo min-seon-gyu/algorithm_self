9d3906bd5cd123d09decc7ad6f28e427e0e926f6
Seongyu <gcael@naver.com>
2024-03-25 12:37:02 +0900

    백준 9251 풀이

---
+++ LCS/백준_9251.java
@@ -0,0 +1,24 @@
+package LCS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준_9251 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int[][] lcs = new int[a.length() + 1][b.length() + 1];
+
+        for(int i = 1 ; i <= a.length(); i++){
+            for(int j = 1 ; j <= b.length(); j++){
+                if(a.charAt(i-1) == b.charAt(j-1)) lcs[i][j] = lcs[i-1][j-1] + 1;
+                if(a.charAt(i-1) != b.charAt(j-1)) lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+            }
+        }
+        System.out.println(lcs[a.length()][b.length()]);
+    }
+
+}
